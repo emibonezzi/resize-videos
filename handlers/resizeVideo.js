3977af7db87b1508cbd7612857ed6ec83d7a818a
emibonezzi <emibonezzi@gmail.com>
2024-09-01 18:46:49 -0400

    Added mediaId for Twitter, improved memory

--- handlers/resizeVideo.js
+++ handlers/resizeVideo.js
@@ -10,15 +10,35 @@ module.exports = async function (req, res, next) {
   try {
     // define new resized path
     req.resizedVideoPath = path.join("/tmp", `${req.fileName}_resized.mp4`);
+    // Define image output path
+    req.imagePath = path.join("/tmp", `${req.fileName}_frame.png`);
+
     // resize
     await new Promise((resolve, reject) => {
       ffmpeg(req.videoPath)
         .size("640x360")
+        .videoFilter(
+          "drawtext=text='TRUZGOALS':fontcolor=white:fontsize=45:x=320:y=40"
+        )
         .save(req.resizedVideoPath)
         .on("end", resolve)
         .on("error", reject);
     });
     console.log("video resized");
+
+    // Save a specific frame as an image
+    await new Promise((resolve, reject) => {
+      ffmpeg(req.videoPath)
+        .screenshots({
+          timestamps: ["82%"], // Capture a frame at 3/4 of the video (82% of the duration)
+          filename: `${req.fileName}_frame.png`, // Name of the output image file
+          folder: "/tmp", // Output folder
+          size: "640x360", // Size of the output image
+        })
+        .on("end", resolve)
+        .on("error", reject);
+    });
+
     next();
   } catch (err) {
     console.error("Error in resizing video...");
--- handlers/uploadVideo.js
+++ handlers/uploadVideo.js
@@ -1,5 +1,15 @@
 const AWS = require("aws-sdk");
 const fs = require("fs");
+const { TwitterApi } = require("twitter-api-v2");
+
+const client = new TwitterApi({
+  appKey: process.env.TWITTER_CONSUMER_KEY,
+  appSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_TOKEN_SECRET,
+});
+
+const rwClient = client.readWrite;
 
 // Configure AWS SDK
 const s3 = new AWS.S3();
@@ -16,10 +26,28 @@ module.exports = async function (req, res, next) {
     };
     const uploadResult = await s3.upload(uploadParams).promise();
     console.log("video uploaded");
+
+    // Upload screenshot to S3
+    const screenshotUploadParams = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: `${req.fileName}_frame.png`,
+      Body: fs.createReadStream(req.imagePath),
+      ContentType: "image/png",
+    };
+    const screenshotUploadResult = await s3
+      .upload(screenshotUploadParams)
+      .promise();
+    console.log("screenshot uploaded");
+
+    // Upload the image to Twitter
+    const mediaId = await rwClient.v1.uploadMedia(req.imagePath);
+
     // Send back url
     res.json({
       original_url: req.body.video_url,
       resizedVideoUrl: uploadResult.Location,
+      screenshotUrl: screenshotUploadResult.Location,
+      mediaIdTwitter: mediaId,
     });
   } catch (err) {
     next(err);
